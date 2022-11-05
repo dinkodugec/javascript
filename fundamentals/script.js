464f5e4ada49b1e41561aec845e1894346af00d6
Dinko <dinko.dugec@gmail.com>
2022-11-05 21:42:26 +0100

    switch statement

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -63,7 +63,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 }
  */
 
-const teamDolphins = 96 + 108 + 89;
+/* const teamDolphins = 96 + 108 + 89;
 console.log(teamDolphins);
 const teamKoalas = 88 + 91 + 110;
 console.log(teamKoalas);
@@ -86,4 +86,47 @@ if (avarageDolphins > avarageKoalas && avarageDolphins > 100) {
   console.log("they are good");
 } else {
   console.log("no one win");
+} */
+
+// The switch Statement
+const day = "monday";
+
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break; //without break code is not stop, just like in php
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+
+//this is same just in switch statement
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
 }
